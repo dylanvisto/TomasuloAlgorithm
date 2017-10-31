efc765523e41c9587b4adb35772a28ad44dac8f6
Dylan <Dylan@DESKTOP-0FE6J80>
2017-10-31 11:07:24 -0500

    New Functionality: Issue Done and Partial Execution finished

--- src/tomasulosalgo/PrintFunctions.java
+++ src/tomasulosalgo/PrintFunctions.java
@@ -5,27 +5,28 @@ public class PrintFunctions {
     
     public void printRFRAT(int[] RF, String[] RAT){
         
-        System.out.println("   RF                   RAT\n");
-        System.out.println("-----------------------------\n"); 
+        System.out.println("   RF                   RAT");
+        System.out.println("-----------------------------"); 
             for(int i = 0; i < 8; i++){
                 int length = String.valueOf(RF[i]).length();
                 if(length == 2){
-                    System.out.println(i+ ":     " + RF[i] + "      |        " + RAT[i] + "\n");
+                    System.out.println("F" + i + ":     " + RF[i] + "      |        " + RAT[i] + "");
                 }
                 else
-                    System.out.println(i+ ":     " + RF[i] + "       |        " + RAT[i] + "\n");
+                    System.out.println("F" + i + ":     " + RF[i] + "       |        " + RAT[i] + "");
             }
+        System.out.println();
         
     }
     
     public void printRS(ReservationStation[] rs){
-        System.out.println("_________________________________________________________________\n");
-        System.out.println("       Busy     OP       VJ       VK       Qj       Qk       Disp\n");
+        System.out.println("_________________________________________________________________");
+        System.out.println("       Busy     OP       VJ       VK       Qj       Qk       Disp");
         for(int i = 0; i <5; i++){
         System.out.println("RS" + i + "    " + rs[i].toString());
         }
-        System.out.println("_________________________________________________________________\n");
-        
+        System.out.println("_________________________________________________________________");
+        System.out.println();
     }
     
     public void printInstructionQueue(ArrayQueue<Instruction> aq, int numInstr){
@@ -33,21 +34,22 @@ public class PrintFunctions {
         for(int i = 0; i < numInstr; i++){
             switch (aq.observe(i).getOpCode()) {
                 case 0:
-                    System.out.println("Add R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2()+ "\n");
+                    System.out.println("Add R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2());
                     break;
                 case 1:
-                    System.out.println("Sub R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2()+ "\n");
+                    System.out.println("Sub R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2());
                     break;
                 case 2:
-                    System.out.println("Mul R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2()+ "\n");
+                    System.out.println("Mul R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2());
                     break;
                 case 3:
-                    System.out.println("Div R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2()+ "\n");
+                    System.out.println("Div R" + aq.observe(i).getDestOp()+ ", R" + aq.observe(i).getSourceOp1()+ ", R" + aq.observe(i).getSourceOp2());
                     break;
                 default:
                     break;
             }
         }
+        System.out.println();
     }
     
     
--- src/tomasulosalgo/TomasulosAlgo.java
+++ src/tomasulosalgo/TomasulosAlgo.java
@@ -18,17 +18,17 @@ public class TomasulosAlgo {
         String[] RAT = new String[8];
         PrintFunctions pf = new PrintFunctions();
         //-------For Testing Only--------//
-        ReservationStation rs1 = new ReservationStation("1" , "Add", " 3", " 3", "RS1", "RS2", "1");
-        ReservationStation rs2 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
-        ReservationStation rs3 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
-        ReservationStation rs4 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
-        ReservationStation rs5 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
-        ReservationStation[] rsArray = new ReservationStation[5];
-        rsArray[0] = rs1;
-        rsArray[1] = rs2;
-        rsArray[2] = rs3;
-        rsArray[3] = rs4;
-        rsArray[4] = rs5;
+//        ReservationStation rs1 = new ReservationStation("1" , "Add", " 3", " 3", "RS1", "RS2", "1");
+//        ReservationStation rs2 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
+//        ReservationStation rs3 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
+//        ReservationStation rs4 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
+//        ReservationStation rs5 = new ReservationStation(" " , "   ", "  ", "  ", "   ", "   ", " ");
+//        ReservationStation[] rsArray = new ReservationStation[5];
+//        rsArray[0] = rs1;
+//        rsArray[1] = rs2;
+//        rsArray[2] = rs3;
+//        rsArray[3] = rs4;
+//        rsArray[4] = rs5;
         //-------For Testing Only-------//
         try {
             Scanner scanner = new Scanner(new File(fileName));
@@ -50,7 +50,9 @@ public class TomasulosAlgo {
                 RF[j] = scanner.nextInt();   
             }
             System.out.println(Arrays.toString(RF));
-            
+            for(int k = 0; k < 8; k++){
+                RAT[k] = "F" + k;
+            }
         }
         catch(FileNotFoundException ex) {
             System.out.println(
@@ -59,7 +61,7 @@ public class TomasulosAlgo {
         }
         
         //-------------------------------------------------------------
-        pf.printRS(rsArray);
+        //pf.printRS(rsArray);
         pf.printRFRAT(RF, RAT);
         pf.printInstructionQueue(instrHold, numInstr);
         
@@ -163,6 +165,7 @@ public class TomasulosAlgo {
             
             
             //----------------------Start Execution--------------//
+            //Need to add functionality for if the FU is still in use. If it is
             
             for(int c = i-1; c >= 0; c--){
                if((instrHold.observe(i).getSourceOp1() == instrHold.observe(c).getDestOp()) || (instrHold.observe(i).getSourceOp2() == instrHold.observe(c).getDestOp())){
@@ -196,7 +199,7 @@ public class TomasulosAlgo {
                     if((instrHold.observe(p).getOpCode() == 2) || (instrHold.observe(p).getOpCode() == 3)){
                        if(execute[i] >= execute[p] && execute[i] < write[p]){
                            execute[i] = write[p];
-                       }; 
+                       } 
                     }
                 }
             }
@@ -241,7 +244,374 @@ public class TomasulosAlgo {
             System.out.println(Arrays.toString(execute));
             System.out.println(Arrays.toString(write)); 
         
+            
+        //---------------RS/RAT/RF Updating Algorithm------------------//
+        int instrIndexI = 0;
+        int instrIndexE = 0;
+        int instrIndexW = 0;
+        int activeCycle = 0;
+        int sourceOP1 = 0;
+        int sourceOP2 = 0;
+        String vjIntToString;
+        String vkIntToString;
         
+        int rsValue = 1;
+        int rsValue2 = 4;
+        for(int i = 1; i <= numCycles; i++){
+            ReservationStation rs1 = new ReservationStation();
+            ReservationStation rs2 = new ReservationStation();
+            ReservationStation rs3 = new ReservationStation();
+            ReservationStation rs4 = new ReservationStation();
+            ReservationStation rs5 = new ReservationStation();
+            activeCycle = 0;
+            instrIndexI = -1;
+            instrIndexE = -1;
+            instrIndexW = -1;
+            for(int j = 0; j < numInstr; j++){
+                if(issue[j] == i){
+                    
+                    instrIndexI = j;
+                    activeCycle = 1;
+                }
+                else if(execute[j] == i){
+                    
+                    instrIndexE = j;
+                    activeCycle = 1;
+                }
+                else if(write[j] == i){
+                    
+                    instrIndexW = j;
+                    activeCycle = 1;
+                }
+                
+                
+            }
+            if(activeCycle != 1){
+                System.out.println("Cycle " + i + " was empty"); 
+            }
+            
+            //------------Issue Algorithm--------------//
+            
+            if(instrIndexI != -1){
+                
+                if(instrHold.observe(instrIndexI).getOpCode() == 0 || instrHold.observe(instrIndexI).getOpCode() == 1){
+                
+                    if(rsValue == 1){
+
+                        switch (instrHold.observe(instrIndexI).getOpCode()) {
+                            case 0:
+                                rs1.setOp("ADD");
+                                break;
+                            case 1:
+                                rs1.setOp("SUB");
+                                break;
+                        }
+
+                                rs1.setBusy("1");
+                                sourceOP1 = instrHold.observe(instrIndexI).getSourceOp1();
+                                sourceOP2 = instrHold.observe(instrIndexI).getSourceOp2();
+                                if(RAT[sourceOP1].equals("F"+(sourceOP1))){
+                                        vjIntToString = Integer.toString(RF[sourceOP1]);
+                                        rs1.setVj(vjIntToString);
+
+                                }
+                                else{
+
+                                    rs1.setVj("   ");
+
+                                }
+
+                                if(RAT[sourceOP2].equals("F"+(sourceOP2))){
+                                        vkIntToString = Integer.toString(RF[sourceOP2]);
+                                        rs1.setVk(vkIntToString);
+
+                                }
+                                else{
+
+                                    rs1.setVk("   ");
+
+                                }
+                                if(rsValue == 4){
+                                    rsValue = 1;
+                                }
+
+                                RAT[instrHold.observe(instrIndexI).getDestOp()] = "RS" + rsValue;
+
+                                rs1.setQj("   ");
+                                rs1.setQk("   ");
+                                rs1.setDisp(" ");
+                                rsValue++;
+                    }
+                    if(rsValue == 2){
+
+                        switch (instrHold.observe(instrIndexI).getOpCode()) {
+                            case 0:
+                                rs2.setOp("ADD");
+                                break;
+                            case 1:
+                                rs2.setOp("SUB");
+                                break;
+                        }
+
+                                rs2.setBusy("1");
+                                sourceOP1 = instrHold.observe(instrIndexI).getSourceOp1();
+                                sourceOP2 = instrHold.observe(instrIndexI).getSourceOp2();
+                                if(RAT[sourceOP1].equals("F"+(sourceOP1))){
+                                        vjIntToString = Integer.toString(RF[sourceOP1]);
+                                        rs2.setVj(vjIntToString);
+
+                                }
+                                else{
+
+                                    rs2.setVj("   ");
+
+                                }
+
+                                if(RAT[sourceOP2].equals("F"+(sourceOP2))){
+                                        vkIntToString = Integer.toString(RF[sourceOP2]);
+                                        rs2.setVk(vkIntToString);
+
+                                }
+                                else{
+
+                                    rs2.setVk("   ");
+
+                                }
+                                if(rsValue == 4){
+                                    rsValue = 1;
+                                }
+
+                                RAT[instrHold.observe(instrIndexI).getDestOp()] = "RS" + rsValue;
+
+                                rs2.setQj("   ");
+                                rs2.setQk("   ");
+                                rs2.setDisp(" ");
+                                rsValue++;
+                    }
+                    if(rsValue == 3){
+
+                        switch (instrHold.observe(instrIndexI).getOpCode()) {
+                            case 0:
+                                rs3.setOp("ADD");
+                                break;
+                            case 1:
+                                rs3.setOp("SUB");
+                                break;
+                        }
+
+                                rs3.setBusy("1");
+                                sourceOP1 = instrHold.observe(instrIndexI).getSourceOp1();
+                                sourceOP2 = instrHold.observe(instrIndexI).getSourceOp2();
+                                if(RAT[sourceOP1].equals("F"+(sourceOP1))){
+                                        vjIntToString = Integer.toString(RF[sourceOP1]);
+                                        rs3.setVj(vjIntToString);
+
+                                }
+                                else{
+
+                                    rs3.setVj("   ");
+
+                                }
+
+                                if(RAT[sourceOP2].equals("F"+(sourceOP2))){
+                                        vkIntToString = Integer.toString(RF[sourceOP2]);
+                                        rs3.setVk(vkIntToString);
+
+                                }
+                                else{
+
+                                    rs3.setVk("   ");
+
+                                }
+                                if(rsValue == 4){
+                                    rsValue = 1;
+                                }
+
+                                RAT[instrHold.observe(instrIndexI).getDestOp()] = "RS" + rsValue;
+
+                                rs3.setQj("   ");
+                                rs3.setQk("   ");
+                                rs3.setDisp(" ");
+                                rsValue++;
+                    }
+                    
+                }
+                else if(instrHold.observe(instrIndexI).getOpCode() == 2 || instrHold.observe(instrIndexI).getOpCode() == 3){
+                    
+                    if(rsValue2 == 4){
+
+                        switch (instrHold.observe(instrIndexI).getOpCode()) {
+                            case 2:
+                                rs4.setOp("MUL");
+                                break;
+                            case 3:
+                                rs4.setOp("DIV");
+                                break;
+                        }
+
+                                rs4.setBusy("1");
+                                sourceOP1 = instrHold.observe(instrIndexI).getSourceOp1();
+                                sourceOP2 = instrHold.observe(instrIndexI).getSourceOp2();
+                                if(RAT[sourceOP1].equals("F"+(sourceOP1))){
+                                        vjIntToString = Integer.toString(RF[sourceOP1]);
+                                        rs4.setVj(vjIntToString);
+
+                                }
+                                else{
+
+                                    rs4.setVj("   ");
+
+                                }
+
+                                if(RAT[sourceOP2].equals("F"+(sourceOP2))){
+                                        vkIntToString = Integer.toString(RF[sourceOP2]);
+                                        rs4.setVk(vkIntToString);
+
+                                }
+                                else{
+
+                                    rs4.setVk("   ");
+
+                                }
+                                if(rsValue2 == 5){
+                                    rsValue2 = 4;
+                                }
+
+                                RAT[instrHold.observe(instrIndexI).getDestOp()] = "RS" + rsValue2;
+
+                                rs4.setQj("   ");
+                                rs4.setQk("   ");
+                                rs4.setDisp(" ");
+                                rsValue2++;
+                    }
+                    if(rsValue2 == 5){
+
+                        switch (instrHold.observe(instrIndexI).getOpCode()) {
+                            case 0:
+                                rs5.setOp("ADD");
+                                break;
+                            case 1:
+                                rs5.setOp("SUB");
+                                break;
+                        }
+
+                                rs5.setBusy("1");
+                                sourceOP1 = instrHold.observe(instrIndexI).getSourceOp1();
+                                sourceOP2 = instrHold.observe(instrIndexI).getSourceOp2();
+                                if(RAT[sourceOP1].equals("F"+(sourceOP1))){
+                                        vjIntToString = Integer.toString(RF[sourceOP1]);
+                                        rs5.setVj(vjIntToString);
+
+                                }
+                                else{
+
+                                    rs5.setVj("   ");
+
+                                }
+
+                                if(RAT[sourceOP2].equals("F"+(sourceOP2))){
+                                        vkIntToString = Integer.toString(RF[sourceOP2]);
+                                        rs5.setVk(vkIntToString);
+
+                                }
+                                else{
+
+                                    rs5.setVk("   ");
+
+                                }
+                                if(rsValue2 == 6){
+                                    rsValue2 = 4;
+                                }
+
+                                RAT[instrHold.observe(instrIndexI).getDestOp()] = "RS" + rsValue2;
+
+                                rs5.setQj("   ");
+                                rs5.setQk("   ");
+                                rs5.setDisp(" ");
+                                rsValue2++;
+                    }
+                    
+                }
+                
+            }
+            else if(instrIndexI == -1){
+                
+            }
+            
+            //-----------------Issue Algorithm finished---------------//
+            
+            
+            //compare using rsValue and name, use mod? issued in order; find all previous instructions of add/sub and mul/div; increment a seperat
+            //dispatch notice only in same cycle it dispatches?
+            int Opget = 0;
+            int sameInstr = 0;
+            int rsLocation = 0;
+            if(instrIndexE != -1){
+                sameInstr = 0;
+                Opget = instrHold.observe(instrIndexE).getOpCode();
+                
+                if(Opget == 0 || Opget == 1){
+                    for(int h = instrIndexE-1; h >= 0; h--){
+                        if(instrHold.observe(h).getOpCode() == 0 || instrHold.observe(h).getOpCode() == 1){
+                            sameInstr++;
+                            
+                        }
+                    }
+                    
+                    rsLocation = sameInstr % 3;
+                    if(rsLocation == 0){
+                        
+                        rsLocation = 3;
+                        
+                    }
+                    switch(rsLocation){
+                        case 1:
+                            rs1.setDisp("1");
+                        case 2:
+                            rs2.setDisp("1");
+                        case 3:
+                            rs3.setDisp("1");   
+                    }
+                }
+                else if(Opget == 2 || Opget == 3){
+                    for(int h = instrIndexE-1; h >= 0; h--){
+                        if(instrHold.observe(h).getOpCode() == 2 || instrHold.observe(h).getOpCode() == 3){
+                            sameInstr++;
+                            
+                        } 
+                    }
+                    
+                    rsLocation = sameInstr % 2;
+                    if(rsLocation == 0){
+                        
+                        rsLocation = 2;
+                        
+                    }
+                    switch(rsLocation){
+                        case 1:
+                            rs4.setDisp("1");
+                        case 2:
+                            rs5.setDisp("1");  
+                    }
+                }
+                
+                
+                
+                
+                
+              
+                
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+        }
         
         
         
